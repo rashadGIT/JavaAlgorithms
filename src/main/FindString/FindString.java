b7df37b7a63427c31bf985b15e03017fb1e63329
RashadB88 <Rashad.barnett@gmail.com>
2020-10-02 23:49:01 -0500

    Find String

--- src/main/FindString/FindString.java
+++ src/main/FindString/FindString.java
@@ -1,6 +1,7 @@
 package main.FindString;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -11,6 +12,8 @@ public class FindString {
 
         List<String> a = Arrays.asList("Hello", "World");
         List<String> b = Arrays.asList("hi", "Rashad");
+
+        System.out.println(new HashSet<String>(Arrays.asList("ABC".substring(0,3).split(""))).size());
 
         stringArray(a,b).forEach(x -> System.out.println(x ? "Yes" : "No"));
     }
