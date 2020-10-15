6dbb2d2f811a9560cf151210657cc4466ab5fceb
RashadB88 <Rashad.barnett@gmail.com>
2020-10-14 23:28:09 -0500

    Ddpth-firstSearch

--- src/main/DepthFirstSearch/DepthFirstSearch.java
+++ src/main/DepthFirstSearch/DepthFirstSearch.java
@@ -1,41 +1,14 @@
 package main.DepthFirstSearch;
 
+import domain.Node;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class DepthFirstSearch {
     public static void main(String[] args) {
-        Node graph = new Node("A");
-        graph.addChild("B").addChild("C").addChild("D");
-        graph.children.get(0).addChild("E").addChild("F");
-        graph.children.get(2).addChild("G").addChild("H");
-        graph.children.get(0).children.get(1).addChild("I").addChild("J");
-        graph.children.get(2).children.get(0).addChild("K");
+        Node graph = Node.TestNode();
         List <String> array = new ArrayList<>();
         System.out.println(graph.depthFirstSearch(array));
     }
-
-    public static class Node {
-        private String name;
-        public List<Node> children = new ArrayList<>();
-
-        public Node(String name) {
-            this.name = name;
-        }
-
-        public List<String> depthFirstSearch(List<String> array) {
-            array.add(this.name);
-            for (Node aChildren : this.children) {
-                aChildren.depthFirstSearch(array);
-            }
-            return array;
-        }
-
-        public Node addChild(String name) {
-            Node child = new Node(name);
-            children.add(child);
-            return this;
-        }
-    }
-
 }
