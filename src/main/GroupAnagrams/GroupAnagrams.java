00c23cec0e5f500ab8c97a86723fb9c1ebcbd677
RashadB88 <Rashad.barnett@gmail.com>
2020-11-19 15:37:38 -0600

    GroupAnagrams

--- src/main/GroupAnagrams/GroupAnagrams.java
+++ src/main/GroupAnagrams/GroupAnagrams.java
@@ -1,2 +1,21 @@
-package main.GroupAnagrams;public class GroupAnagrams {
+package main.GroupAnagrams;
+
+import java.util.*;
+
+public class GroupAnagrams {
+    public static List<List<String>> groupAnagrams(List<String> words) {
+        Map<String, List<String>> anagrams = new HashMap<>();
+        for(String word : words){
+            char[] charArray = word.toCharArray();
+            Arrays.sort(charArray);
+            String sortedWord = new String(charArray);
+
+            if(anagrams.containsKey(sortedWord)){
+                anagrams.get(sortedWord).add(word);
+            }else{
+                anagrams.put(sortedWord, new ArrayList<>(Collections.singletonList(word)));
+            }
+        }
+        return new ArrayList<>(anagrams.values());
+    }
 }
--- src/test/GroupAnagrams/GroupAnagramsTest.java
+++ src/test/GroupAnagrams/GroupAnagramsTest.java
@@ -1,2 +1,39 @@
-package test.GroupAnagrams;public class GroupAnagramsTest {
+package test.GroupAnagrams;
+
+import main.GroupAnagrams.GroupAnagrams;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GroupAnagramsTest {
+    @Test
+    public void TestCase1() {
+        List<String> words = new ArrayList<>(Arrays.asList("yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"));
+        List<List<String>> expected = new ArrayList<>();
+        expected.add(new ArrayList<>(Arrays.asList("yo", "oy")));
+        expected.add(new ArrayList<>(Arrays.asList("flop", "olfp")));
+        expected.add(new ArrayList<>(Arrays.asList("act", "tac", "cat")));
+        expected.add(new ArrayList<>(Collections.singletonList("foo")));
+        List<List<String>> output = GroupAnagrams.groupAnagrams(words);
+        for (List<String> innerList : output) {
+            Collections.sort(innerList);
+        }
+        assertTrue(compare(expected, output));
+    }
+
+    public boolean compare(List<List<String>> expected, List<List<String>> output) {
+        if (expected.size() != output.size()) return false;
+
+        for (List<String> group : expected) {
+            Collections.sort(group);
+            if (!output.contains(group)) return false;
+        }
+
+        return true;
+    }
 }
